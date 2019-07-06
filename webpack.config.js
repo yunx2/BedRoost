3a054b1d90faae56b0df4d47d8dd1d73b9e733af
Yunyun <celeph4is@gmail.com>
2019-07-06 12:37:47 -0700

    ininitial set up commit, it renders and starts

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+              }
+            }
+          }
+        ]
+      },
+resolve: {
+extensions: ['*', '.js', '.jsx']
+},
+output: {
+path: __dirname + '/dist',
+publicPath: '/',
+filename: 'bundle.js'
+}
+};
