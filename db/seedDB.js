3afd3c7e93b8be8aa238cc271daff00ee26199b1
Yunyun <celeph4is@gmail.com>
2019-07-07 20:44:43 -0700

    use faker library to  generate mock listing descriptions, add documents to db according to schema

--- db/seedDB.js
+++ db/seedDB.js
@@ -3,12 +3,9 @@ const faker = require('faker');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/description');
-const randomName = faker.name.findName(); // Rowan Nikolaus
 
 const db = mongoose.connection;
 const { Schema } = mongoose;
-
-db.on('error', () => console.log('connection error'));
 
 const descriptionSchema = new Schema({
   _id: Number,
@@ -19,40 +16,67 @@ const descriptionSchema = new Schema({
   guestsSay: { type: Map, of: String },
   selfCheckIn: Boolean,
   description: String,
-  Amenities: { type: Map, of: [{}] },
+  Amenities: { type: Map, of: {} },
   city: String,
   sleepingArrangements: String,
 });
 
 const Description = mongoose.model('Description', descriptionSchema);
-
-for (let i = 0; i < 100; i += 1) {
-  const randomHometype = faker.random.arrayElement(['entire house', 'entire apartment', 'private room', 'shared room']);
+const homeTypes = ['entire house', 'entire apartment', 'private room', 'shared room'];
+// eslint-disable-next-line prefer-template
+const guestComments = {
   // eslint-disable-next-line prefer-template
-  const randomGuestComments = faker.random.arrayElement([{ 'Great Location': faker.random.number({ min: 80, max: 100 }) + ' % of recent guests gave the location a 5-star rating.' }, { 'Sparkling Clean': faker.random.number({ min: 10, max: 25 }) + ' recent guests said this place was sparkling clean.' }, { 'Great Location': faker.random.number({ min: 80, max: 100 }) + ' % of recent guests gave the location a 5-star rating.', 'Sparkling Clean': faker.random.number({ min: 10, max: 25 }) + ' recent guests said this place was sparkling clean.' }]);
+  'Great Location': faker.random.number({ min: 80, max: 100 }) + ' % of recent guests gave the location a 5-star rating.',
+  // eslint-disable-next-line prefer-template
+  'Sparkling Clean': faker.random.number({ min: 10, max: 25 }) + ' recent guests said this place was sparkling clean.',
+  // eslint-disable-next-line prefer-template
+  'Great Check-in experience': faker.random.number({ min: 80, max: 100 }) + ' % of recent guests gave the check-in process a 5-star rating.',
+};
+/* amenity types */
+const basic = {
+  wifi: 'Continuous access in the listing',
+  'Laptop friendly workspace': 'A table or desk with space for a laptop and a chair that’s comfortable to work in',
+  essentials: 'Towels, bed sheets, soap, and toilet paper',
+};
 
-  const 
+const dining = {
+  kitchen: 'Space where guests can cook their own meals',
+  'coffee maker': 'coffee maker',
+  'cooking basics': 'Pots and pans, oil, salt and pepper',
+};
+const notIncluded = ['air conditioning', 'private entrance'];
 
-  Description.create({
-    id: faker.random.number({ min: 100000, max: 200000 }),
-    title: faker.hacker.phrase(),
-    host: { name: faker.internet.userName(), superhost: faker.random.boolean() },
-    hometype: {
-      hometype: randomHometype, guests: faker.random.number({ min: 1, max: 5 }), beds: faker.random.number({ min: 0, max: 5 }), baths: faker.random.number({ min: 0, max: 5 }) 
-    },
-    // roomtype: String,
-    guestsSay: randomGuestComments,
-    selfCheckIn: faker.random.boolean(),
-    description: faker.lorem.paragraph(),
-    Amenities: { type: Map, of: [{}] },
-    city: faker.address.city(),
-    sleepingArrangements: String
-  })
+const getRandomObjectElements = (obj) => {
+  const elements = {};
+  const entries = Object.entries(obj);
+  const numOfElements = faker.random.number(entries.length);
+  for (let i = 0; i < numOfElements; i += 1) {
+    const element = faker.random.arrayElement(entries);
+    const [key, value] = element;
+    elements[key] = value;
+  }
+  return elements;
+};
+
+async function seedDB() {
+  const documents = [];
+  for (let i = 0; i < 100; i += 1) {
+    documents.push(Description.create({
+      id: faker.random.number({ min: 100000, max: 200000 }),
+      title: faker.hacker.phrase(),
+      host: { name: faker.internet.userName(), superhost: faker.random.boolean() },
+      hometype: {
+        hometype: faker.random.arrayElement(homeTypes), guests: faker.random.number({ min: 1, max: 5 }), beds: faker.random.number({ min: 0, max: 5 }), baths: faker.random.number({ min: 0, max: 5 }) 
+      },
+      guestsSay: getRandomObjectElements(guestComments),
+      selfCheckIn: faker.random.boolean(),
+      description: faker.lorem.paragraph(),
+      Amenities: { basic: getRandomObjectElements(basic), dining: getRandomObjectElements(dining), notIncluded: getRandomObjectElements(notIncluded) },
+      city: faker.address.city(),
+      sleepingArrangements: String,
+    }));
+  }
+  await Promise.all(documents);
 }
-
-
-
-var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-var randomCard = faker.helpers.createCard();
-
-console.log(randomName);
+db.on('error', () => console.log('connection error'));
+db.once('open', () => seedDB());
