812d130650e46de9fb322e9f39bb751c2d06a467
Yunyun <celeph4is@gmail.com>
2019-07-08 18:57:44 -0700

    edit linter rules, create express server

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,6 @@ module.exports={
       "max-len": [1, 120, 2, {ignoreComments: true}],
       "no-unused-vars": [1, {"vars": "local", "args": "none"}],
       "no-param-reassign": 0,
+      "no-console": 0,
     }
    }
---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`listening at port ${port}`);
+  }
+});
